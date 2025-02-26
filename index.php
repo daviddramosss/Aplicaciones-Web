09a04b9aba033e3ffaf4427887d52b1c63a3b5b7
David Ramos <davidramosdelucas@gmail.com>
2025-02-26 18:05:10 +0100

    Segunda modifcacion de index

--- index.php
+++ index.php
@@ -3,10 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="CSS/styles.css>
+    <title>Market Chef</title>
+    <link rel="stylesheet" href="CSS/styles.css"> 
 </head>
 <body>
+    <div id="contenedor"> 
     
+    <!-- Plantilla de la página web --> 
+    <?php include 'includes/vistas/comun/plantilla.php'; ?>
+
+    <!-- Parte central de la página web -->
+    <main> 
+        <h1>Market Chef</h1> 
+
+        <p>Top 10 recetas de hoy</p>
+        <!-- Aqui se mostrará más adelante las recetas del dia de hoy -->
+
+        
+
+    </main>
+
+    
+
+    
+
+
+    
+
+
+
+
+    </div>
 </body>
 </html>
