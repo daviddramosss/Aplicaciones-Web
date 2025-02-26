254cd8cf956233740d7dcf4d784a330209697065
Antonio Lopez <antonl11@ucm.es>
2025-02-26 18:20:34 +0100

    SRC 2.1

--- includes/config.php
+++ includes/config.php
@@ -8,9 +8,8 @@ define('RUTA_APP', '/MarketChef');
 define('RUTA_IMGS', RUTA_APP . '/img');
 define('RUTA_CSS', RUTA_APP . '/CSS');
 define('RUTA_JS', RUTA_APP . '/js');
-define('INSTALADA', true);
 
-// Parámetros de configuración de la BD
+// Parámetros de configuración de la Base de Datos
 define('BD_HOST', 'localhost');
 define('BD_NAME', 'root');
 define('BD_USER', 'root');
@@ -21,14 +20,6 @@ define('BD_PASS', 'root');
 /* */
 
 require_once __DIR__.'/src/Utils.php';
-
-/* */
-/* Inicialización de la aplicación */
-/* */
-
-if (!INSTALADA) {
-	Utils::paginaError(502, 'Error', 'Vaya...', 'La aplicación no está configurada debes de modificar el fichero config.php');
-}
 
 /* */
 /* Configuración de Codificación y timezone */
@@ -42,28 +33,13 @@ date_default_timezone_set('Europe/Madrid');
 /* Clases y Traits de la aplicación */
 /* */
 require_once 'src/Arrays.php';
-require_once 'src/traits/MagicProperties.php';
-
-/* */
-/* Clases que simulan una BD almacenando los datos en memoria */
-/*
-require_once 'src/usuarios/memoria/Usuario.php';
-require_once 'src/mensajes/memoria/Mensaje.php';
-*/
 
 /*
  * Configuramos e inicializamos la sesión para todas las peticiones
  */
 session_start([
-	'cookie_path' => RUTA_APP, // Para evitar problemas si tenemos varias aplicaciones en htdocs
+	'cookie_path' => RUTA_APP, 
 ]);
-
-/* */
-/* Inicialización de las clases que simulan una BD en memoria */
-/*
-Usuario::init();
-Mensaje::init();
-*/
 
 /* */
 /* Clases que usan una BD para almacenar el estado */
