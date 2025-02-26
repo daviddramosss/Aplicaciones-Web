d2e2d1e55882f14427a049c2868f50d5a4f09a2f
Jose Pavón <jpavonm03#hotmail.com>
2025-02-26 18:31:18 +0100

    Cabecera y css

--- includes/vistas/comun/cabecera.php
+++ includes/vistas/comun/cabecera.php
@@ -10,23 +10,23 @@
     <link rel="stylesheet" href="CSS/cabecera.css">
 </head>
 <body>
-    <header class="header">
-        <div class="container">
+    <header class="header_cabecera">
+        <div class="container_cabecera">
            <!-- Buscador -->
-           <form class="search-form" action="buscar.php" method="GET">
-                <input class="search-input" type="search" placeholder="Buscar..." name="q" aria-label="Buscar">
-                <button class="search-button" type="submit">Buscar</button>
+           <form class="search-form_cabecera" action="buscar.php" method="GET">
+                <input class="search-input_cabecera" type="search" placeholder="Buscar..." name="q" aria-label="Buscar">
+                <button class="search-button_cabecera" type="submit">Buscar</button>
             </form>
-            
+
             <!-- Logo -->
-            <a href="index.php" class="logo">
+            <a href="index.php" class="logo_cabecera">
             <img src="img/Logo.png" alt="Home">
             </a>
             
            
             
             <!-- Botón de Ayuda -->
-            <a href="ayuda.php" class="help-button">Ayuda</a>
+            <a href="ayuda.php" class="help-button_cabecera">Ayuda</a>
         </div>
     </header>
 </body>
