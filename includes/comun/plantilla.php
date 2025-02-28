8449fc94179e9fa58c2749c42bc9d8486e295d01
Jose Pavón <jpavonm03#hotmail.com>
2025-02-28 19:43:50 +0100

    Plantilla dinamica y su css

--- includes/comun/plantilla.php
+++ includes/comun/plantilla.php
@@ -3,11 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $tituloPagina ?></title>
 </head>
 <body>
 
-<?php include 'cabecera.php'; ?>
+    <?php include 'cabecera.php'; ?>
 
+    <div class="contenido_plantilla">
+        <!-- El contenido principal va aquí -->
+        <h2>Bienvenido a la tienda</h2>
+        <p>Contenido dinámico de la tienda...</p>
+    </div>
+
+    <?php include 'pie.php'; ?>
 
 
 </body>
