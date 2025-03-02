4201053e09c37468b798824b17bfc08201733b4c
Fran Sanchez de Leon <fransanchezdeleon@gmail.com>
2025-03-02 16:23:46 +0100

    Reorganizacion contenido de usuario

--- includes/vistas/helpers/loginForm.php
+++ includes/vistas/helpers/loginForm.php
@@ -1,7 +1,7 @@
 <?php
 
-include __DIR__ . "/../comun/formBase.php";
-include __DIR__ . "/../usuario/userAppService.php";
+include __DIR__ . "/../../comun/formularioBase.php";
+include __DIR__ . "/../../usuario/userAppService.php";
 
 class loginForm extends formularioBase
 {
