298be7151b5a1f0356b016a8472ec6dbc14c862c
Jose Pavón <jpavonm03#hotmail.com>
2025-03-05 23:11:04 +0100

    gestionarUsuarios comentado

--- gestionarUsuarios.php
+++ gestionarUsuarios.php
@@ -3,10 +3,13 @@
 
 require_once("includes/config.php");
 
+// Página de Gestionar usuarios, exclusiva para el rol Admin
 $tituloPagina = 'Gestionar Usuarios';
 
+// Guardamos el contenido principal de la página en la variable dinámica que gestiona la plantilla para cargar el contenido
 $contenidoPrincipal = <<<EOS
 
+     <!-- Breve introducción provisional de cómo funcionará la página cuando se implemente -->
     <h2>Panel de administración de usuarios</h2>
     <p>En este panel puedes gestionar los usuarios de la aplicación.</p>
     <p>Vas a poder borrar, crear, o editar los usuarios que quieras</p>
