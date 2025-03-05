75099bf7c2d86d9eaa09c857f19e3bf63e9a1856
Jose Pavón <jpavonm03#hotmail.com>
2025-03-05 23:42:05 +0100

    loginForm comentado

--- includes/helpers/loginForm.php
+++ includes/helpers/loginForm.php
@@ -3,6 +3,8 @@
 include __DIR__ . "/../comun/formularioBase.php";
 include __DIR__ . "/../usuario/userAppService.php";
 
+// archivo en el que encontramos el formulario de login
+
 class loginForm extends formularioBase
 {
     public function __construct() 
@@ -10,10 +12,12 @@ class loginForm extends formularioBase
         parent::__construct('loginForm');
     }
     
+    // función que se encarga de crear los campos en los que el usuario debe introducir su email y contraseña para iniciar sesión
     protected function CreateFields($datos)
     {
         $email = '';
         
+        // si el usuario ha fallado en el incio de sesión, al volver a cargar la página, le dejaremos el campo de email relleno con su anterior introducción
         if ($datos) 
         {
             $email = isset($datos['email']) ? $datos['email'] : $email;
@@ -26,44 +30,53 @@ class loginForm extends formularioBase
             <p><label>Password:</label> <input type="password" name="password" /></p>
             <button type="submit" name="login">Entrar</button>
         </fieldset>
-EOF;
+        EOF;
+
         return $html;
     }
     
-
+    // función que se encarga de procesar los datos que ha insertado el usuario
     protected function Process($datos)
     {
         $result = array();
         
         //filter_var vs htmlspecialchars(trim(strip_tags($_REQUEST["username"])));
 
+        // guardamos el email y comprobamos que tiene formato de email
         $email = trim($datos['email'] ?? '');
         
-        $email = filter_var($email, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
                 
+        // si el campo está vacío cuando se intenta acceder, guardamos un error
         if ( empty($email) ) 
         {
             $result[] = "El email no puede estar vacío";
         }
         
+        // guardamos la contraseña y comprobamos que está bien escrita
         $password = trim($datos['password'] ?? '');
         
         $password = filter_var($password, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
+        // si el campo está vacío cuando se intenta acceder, guardamos un error
         if ( empty($password) ) 
         {
             $result[] = "El password no puede estar vacío.";
         }
         
+        // si no ha habido ningun error
         if (count($result) === 0) 
         {
+            // creamos un usuario con el email y la contraseña pasados por el usuario
             $userDTO = new userDTO(0, '', '', $email, '',$password, 
             '', '');
 
+            // creamos la instancia de userAppService y llamamos a la función login
             $userAppService = userAppService::GetSingleton();
 
             $foundedUserDTO = $userAppService->login($userDTO);
 
+            // Si algo no coincide con la base de datos y no ha podido iniciar sesión, guardamos error
             if ( ! $foundedUserDTO ) 
             {
                 // No se da pistas a un posible atacante
@@ -71,12 +84,16 @@ EOF;
             } 
             else 
             {
+                // si ha iniciado sesión, se almacenan los valores relevantes de la sesión
                 $_SESSION["login"] = true;
                 $_SESSION["usuario"] = $email;
 
+
+                // se guarda la ruta a la que se debe redirigir al usuario
                 $result = 'index.php';
             }
         }
+        // devolvemos el array con, o bien los errores, o bien la ruta a la que debemos redirigir al usuario
         return $result;
     }
 }
