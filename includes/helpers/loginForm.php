cd5ce1fae8365a5f4e7c1abbc1c523ad5a117c91
Fran Sanchez de Leon <fransanchezdeleon@gmail.com>
2025-03-06 15:55:12 +0100

    loginForm 1.5 (Bug include_once)

--- includes/helpers/loginForm.php
+++ includes/helpers/loginForm.php
@@ -1,7 +1,7 @@
 <?php
 
 include __DIR__ . "/../comun/formularioBase.php";
-include __DIR__ . "/../usuario/userAppService.php";
+include_once __DIR__ . "/../usuario/userAppService.php";
 
 // archivo en el que encontramos el formulario de login
 
