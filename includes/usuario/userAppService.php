3d42d5bf7841d9c7c83a8c2b680b7ca3670507c9
Jose Pavón <jpavonm03#hotmail.com>
2025-03-06 15:57:27 +0100

    userAppService comentado

--- includes/usuario/userAppService.php
+++ includes/usuario/userAppService.php
@@ -3,10 +3,13 @@
 require_once("userFactory.php");
 require_once("userDTO.php");
 
+// Clase intermedia mediante la cual podemos acceder a las operaciones relacionadas con los usuarios
 class userAppService
 {
+    // Declaramos los atributos que tendrá la clase
     private static $instance;
 
+    //  Función que devuleve la Singleton de la clase
     public static function GetSingleton()
     {
         if (!self::$instance instanceof self)
@@ -21,6 +24,7 @@ class userAppService
     {
     } 
 
+    // Función mediante la cual accedemos a donde está la lógica del login y obtenemos el usuario de la base de datos
     public function login($userDTO)
     {
         $IUserDAO = userFactory::CreateUser();
@@ -30,6 +34,7 @@ class userAppService
         return $foundedUserDTO;
     }
 
+    // función mediante la cual accedemos a las funciones que crean al usuario en la base de datos y devolvemos el usuario creado
     public function create($userDTO)
     {
         $IUserDAO = userFactory::CreateUser();
@@ -39,7 +44,7 @@ class userAppService
         return $createdUserDTO;
     }
 
-        
+    // función mediante la cual accedemos a la lógica detrás de la función que busca al usuario en la base de datos por su email
     public function buscarUsuario($email)
     {
         $IUserDAO = userFactory::CreateUser();
