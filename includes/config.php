091b356320c4686aa0535f861d06c78dd6017409
Jose Pavón <jpavonm03#hotmail.com>
2025-03-25 18:55:29 +0100

    Función de autocarga del config

--- includes/config.php
+++ includes/config.php
@@ -11,6 +11,43 @@ ini_set('default_charset', 'UTF-8');
 setLocale(LC_ALL, 'es_ES.UTF.8');
 date_default_timezone_set('Europe/Madrid');
 
+
+
+/**
+ * Función para autocargar clases PHP.
+ *
+ * @see http://www.php-fig.org/psr/psr-4/
+ */
+spl_autoload_register(function ($class) {
+    
+    // project-specific namespace prefix
+    $prefix = 'es\\ucm\\fdi\\aw\\';
+    
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/';
+    
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+    
+    // get the relative class name
+    $relative_class = substr($class, $len);
+    
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+
+
 $app = application::getInstance();
 
 $app->init(array('host'=>BD_HOST, 'bd'=>BD_NAME, 'user'=>BD_USER, 'pass'=>BD_PASS));
