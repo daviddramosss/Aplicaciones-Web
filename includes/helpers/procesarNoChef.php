c6dc3db5479407f8ddd247cae18bf9c5b8427a58
antoniolopez03 <antonl11@ucm.es>
2025-03-25 11:10:52 +0100

    ZIP

--- includes/helpers/procesarNoChef.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $dni = htmlspecialchars($_POST["dni"]);
-        $cuentaBancaria = htmlspecialchars($_POST["cuentaBancaria"]);
-
-        header("Location: index.php");
-        exit();
-    }
-?>
