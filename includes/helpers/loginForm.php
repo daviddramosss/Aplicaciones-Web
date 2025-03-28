38cb7ca4bcfd246201f3886430ffb91291b5d870
Fran Sanchez de Leon <fransanchezdeleon@gmail.com>
2025-03-28 16:01:53 +0100

    estiloGeneral 1.3.2 (loginForm correccion)

--- includes/helpers/loginForm.php
+++ includes/helpers/loginForm.php
@@ -33,7 +33,7 @@ class loginForm extends formularioBase
                 <div class="input-container"><input type="text" name="email" placeholder="EMAIL" value="$email" required/></div>
                 <p> </p>
                 <div class="input-container"><input type="password" name="password" placeholder="CONTRASEÑA" required/></div>
-
+                <p> </p>
                 <button type="submit" name="login">Entrar</button>
             </fieldset>
         EOF;
