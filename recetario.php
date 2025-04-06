d3b1a0c0649e0ede7f61c4e8ea65accbb89af0d0
Jose Pavón <jpavonm03#hotmail.com>
2025-04-07 00:36:56 +0200

    Cambiado index.css a recetario.css

--- recetario.php
+++ recetario.php
@@ -8,7 +8,7 @@ $tituloPagina = 'Market Chef';
 // Define el contenido principal de la página, que será insertado en la plantilla
 $contenidoPrincipal = <<<EOS
     <!-- Enlace a la hoja de estilos específica para esta página -->
-    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="CSS/recetario.css">
     
     <h1> Mi recetario </h1>
 
