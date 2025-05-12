41d7654766e218b5c1a664c1bf9a838de0b7b5f8
Fran Sanchez de Leon <fransanchezdeleon@gmail.com>
2025-05-12 19:16:45 +0200

    codigo de error corregido

--- includes/comun/formularioBase.php
+++ includes/comun/formularioBase.php
@@ -69,7 +69,7 @@ abstract class formularioBase
         
         return $html;
     }
-
+/*
     private function CreateErrors($errores)
     {
         $html='';
@@ -86,6 +86,19 @@ abstract class formularioBase
         }
         return $html;
     }
+*/
+    private function CreateErrors($errores)
+{
+    $html = '';
+    $numErrores = count($errores);
+    if ($numErrores > 0) {
+        $html .= '<div class="error">';
+        $html .= implode('<br>', $errores);
+        $html .= '</div>';
+    }
+    return $html;
+}
+
 
     protected function CreateFields($datosIniciales)
     {
